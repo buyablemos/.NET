aeeae3d21d722d03890d3d4d909de20598b77458
Dawid <Dawid@LAPTOP-M4II16HF>
2024-02-28 21:45:20 +0100

    Dodaj pliki projektów.

---
+++ LAB0/pierwszy/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace pierwszy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fz = new FizzBuzz();
+            fz.Read();
+            fz.Display();
+        }
+    }
+}
